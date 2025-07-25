b109960c73f643320f755146ba659e5a682d4168
LOTTEWORLD\dev_0156 <dev_0156@lotte.net>
2025-07-25 15:43:34 +0900

    2-4. 컴포넌트 분리와 재사용 - 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import "./App.css";
-import UserList from "./components/UserList";
+import ProductList from './components/ProductList';
 
 function App() {
-    return (
-        <div>
-            <h1>React 컴포넌트 분리 예제</h1>
-            <UserList />
-        </div>
-    )
+  return (
+    <div>
+      <h1>React 컴포넌트 분리 & 재사용 예제</h1>
+      <ProductList />
+    </div>
+  );
 }
 
 export default App;
