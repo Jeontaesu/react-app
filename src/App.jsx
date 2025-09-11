a070d218da68f57f45187658f0081cdc08187745
Jeontaesu <jeontaesu@gmail.com>
2025-09-12 00:25:23 +0900

    props 예제&과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import UserCard from "./components/UserCard";
+
 function App() {
-    return <>111</>;
+    return (
+        <div>
+            <h1>👋 React Props 예제</h1>
+            <UserCard name="홍길동" age={30} />
+            <UserCard name="전태수" age={24} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,7 @@
+export default function UserCard({ name, age }) {
+    return (
+        <div>
+            {name}님은 {age}입니다.
+        </div>
+    );
+}
