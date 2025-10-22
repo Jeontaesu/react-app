ea73c4eff45d8f2871c5f33b9a68be619720733f
Jeontaesu <jeontaesu@gmail.com>
2025-10-22 23:54:07 +0900

    useState 타이머 만들기(예제)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import MultiCounter from "./components/MultiCounter";
+// import MultiCounter from "./components/MultiCounter";
+
+import Timer from "./components/Timer";
 
 function App() {
     return (
         <div>
             <h1>useState 예제</h1>
-            <MultiCounter />
+            {/* <MultiCounter /> */}
+            <Timer />
         </div>
     );
 }
